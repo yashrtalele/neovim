4550e6c2d008db74f4214f516b21f0bbf3c15c51
Yash Talele <yashrtalele@gmail.com>
2025-05-25 11:27:17 +0530

    fix: fixed transparency for floats and telescope

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,4 +1,48 @@
 return {
   "xiyaowong/transparent.nvim",
-  lazy = false
+  lazy = false,
+  config = function()
+    require("transparent").setup({
+      extra_groups = {
+        -- Fidget
+        "FidgetTask",
+        "FidgetTitle",
+        "FidgetWindow",
+
+        -- LSP floating windows
+        "NormalFloat",
+        "FloatBorder",
+        "LspInfoBorder",
+
+        -- Lint / Diagnostics
+        "DiagnosticFloatingError",
+        "DiagnosticFloatingWarn",
+        "DiagnosticFloatingInfo",
+        "DiagnosticFloatingHint",
+        "DiagnosticVirtualTextError",
+        "DiagnosticVirtualTextWarn",
+        "DiagnosticVirtualTextInfo",
+        "DiagnosticVirtualTextHint",
+
+        -- You might want this if you're using borders with LSP UIs
+        "DiagnosticSignError",
+        "DiagnosticSignWarn",
+        "DiagnosticSignInfo",
+        "DiagnosticSignHint",
+
+        -- Completion popups (e.g., nvim-cmp)
+        "Pmenu",
+        "PmenuSel",
+        "PmenuSbar",
+        "PmenuThumb",
+
+        "NotifyBackground", -- base window background
+        "NotifyERRORBody",
+        "NotifyWARNBody",
+        "NotifyINFOBody",
+        "NotifyDEBUGBody",
+        "NotifyTRACEBody",
+      },
+    })
+  end,
 }
