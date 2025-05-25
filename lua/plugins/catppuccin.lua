eeb036c1a6a17f458ba77a23b3ed5f645cb56f3b
Yash Talele <yashrtalele@gmail.com>
2025-05-25 11:26:18 +0530

    chore: added transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,31 +1,35 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("catppuccin")
-    require("catppuccin").setup({
-      flavour = "auto",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      -- transparent_background = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = true,
-        telescope = {
-          enabled = true,
-          style = "nvchad",
-        },
-        mini = {
-          enabled = true,
-          indentscope_color = "",
-        },
-      },
-    })
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		transparent = true,
+		transparent_background = true,
+	},
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+		require("catppuccin").setup({
+			flavour = "auto",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = true,
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+		})
+	end,
 }
