5108fbcb503b50ede71ffe0a1b27d07af0fd5395
Yash Talele <yashrtalele@gmail.com>
2025-05-25 10:29:07 +0530

    feat: added conform
    Added conform to help format the files

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,34 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>fm",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      desc = "File format",
+    },
+  },
+  opts = {
+    lsp_fallback = true,
+
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      sh = { "shfmt" },
+      go = { "goimports", "gofumpt" },
+    },
+
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
